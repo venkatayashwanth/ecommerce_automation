70fdd4c0579816d26db1af2fd54fe3d68e51719b
venkatayashwanth <yashwanthvenkata77777@gmail.com>
2025-07-23 12:14:48 +0530

    increased wait time to click checkout button

--- actions/cart_actions.py
+++ actions/cart_actions.py
@@ -1,6 +1,10 @@
 from actions.actions import Actions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class CartActions(Actions):
     def click_checkout(self):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable(self.locators.checkout_button))
         self.click(self.locators.checkout_button)
