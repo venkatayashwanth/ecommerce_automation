ffdc4296b052aa024ea7f91aad595761d0bb6c2e
venkatayashwanth <yashwanthvenkata77777@gmail.com>
2025-07-23 15:29:10 +0530

    no headless

--- conftest.py
+++ conftest.py
@@ -30,7 +30,7 @@ def driver(request):
         options.add_experimental_option("prefs", prefs)
         options.add_argument("--log-level=3")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        options.add_argument("--headless")
+        #options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument('--disable-dev-shm-usage')
