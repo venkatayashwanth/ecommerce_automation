c04523b832b486907e7c3998ce654c585cb9c1d6
venkatayashwanth <yashwanthvenkata77777@gmail.com>
2025-07-23 15:05:06 +0530

    changed pytest.yml

--- conftest.py
+++ conftest.py
@@ -39,7 +39,7 @@ def driver(request):
 
     elif browser == "firefox":
         options = FirefoxOptions()
-        options.add_argument("--headless")
+        #options.add_argument("--headless")
         options.add_argument("--width=1920")
         options.add_argument("--height=1080")
         driver = webdriver.Firefox(options=options)
